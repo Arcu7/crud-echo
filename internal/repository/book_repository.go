872a5a3b469526bca38641910a0153dc9465f57d
Administrator <gitlab_admin_c1ee3c@example.com>
2025-04-08 16:59:54 +0700

    refactored repo code for correct error handling

--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -16,20 +16,23 @@ func NewBooksRepository(db *gorm.DB) *BooksRepository {
 
 func (r *BooksRepository) Create(book *models.Books) error {
 	result := r.DB.Create(&book)
+
 	if result.Error != nil {
 		return result.Error
 	}
+
 	return nil
 }
 
 func (r *BooksRepository) GetByID(book *models.Books, id int) error {
 	result := r.DB.First(&book, id)
-	if result.Error != nil {
-		return result.Error
-	}
 
-	if result.RowsAffected == 0 {
-		return models.ErrNotFound
+	if result.Error != nil {
+		// have to do this?
+		if result.Error == gorm.ErrRecordNotFound {
+			return models.ErrNotFound
+		}
+		return result.Error
 	}
 
 	return nil
@@ -37,14 +40,13 @@ func (r *BooksRepository) GetByID(book *models.Books, id int) error {
 
 func (r *BooksRepository) GetAll(books *models.BooksList) error {
 	result := r.DB.Find(&books)
+
 	if result.Error != nil {
 		return result.Error
-	}
-
-	// maybe only need to do just 1 check instead
-	if result.RowsAffected == 0 || len(*books) == 0 {
+	} else if result.RowsAffected < 1 || len(*books) == 0 { // maybe only need to do one check
 		return models.ErrTableEmpty
 	}
+
 	return nil
 }
 
@@ -54,26 +56,35 @@ func (r *BooksRepository) Update(book *models.Books) error {
 		Description: book.Description,
 		Qty:         book.Qty,
 	})
+
 	if result.Error != nil {
 		return result.Error
-	}
-	if result.RowsAffected == 0 {
-		return gorm.ErrRecordNotFound
+	} else if result.RowsAffected < 1 {
+		return models.ErrNotFound
 	}
 
 	return nil
 }
 
 func (r *BooksRepository) Delete(book *models.Books) error {
-	result := r.DB.Delete(&book).Error
-	return result
+	result := r.DB.Delete(&book)
+
+	if result.Error != nil {
+		return result.Error
+	} else if result.RowsAffected < 1 {
+		return models.ErrNotFound
+	}
+
+	return nil
 }
 
 func (r *BooksRepository) ExistsByTitle(title string) (bool, error) {
 	var count int64
 	result := r.DB.Model(&models.Books{}).Where("title = ?", title).Count(&count)
+
 	if result.Error != nil {
 		return false, result.Error
 	}
+
 	return count > 0, nil
 }
