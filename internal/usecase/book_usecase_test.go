52b6c66b0523d9e52f1d0806e45989ca0ed68d0f
Administrator <gitlab_admin_c1ee3c@example.com>
2025-04-08 17:01:23 +0700

    added usecase test (all func except update and delete)

--- internal/usecase/book_usecase_test.go
+++ internal/usecase/book_usecase_test.go
@@ -2,48 +2,97 @@ package usecase
 
 import (
 	"crud-echo/internal/models"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
 
-// Helper function to create test create book request data
-func createTestCreateBookRequest(title string, description string, qty int) *models.CreateBooksRequest {
-	return &models.CreateBooksRequest{
-		Title:       title,
-		Description: description,
-		Qty:         qty,
-	}
-}
+var timeNow = time.Now
 
 func TestCreateBook(t *testing.T) {
 	tests := []struct {
 		name        string
 		bookRequest *models.CreateBooksRequest
+		expectedID  int
 		mock        func(mock *MockusecaseBooksRepository)
 		wantErr     bool
 		errType     error
-		expectedID  int
 	}{
 		{
-			name:        "Success create book with valid data",
-			bookRequest: createTestCreateBookRequest("Test Title", "Test Description", 10),
-			expectedID:  1,
+			name: "Success create book with valid data",
+			bookRequest: &models.CreateBooksRequest{
+				Title:       "Test Title",
+				Description: "Test Description",
+				Qty:         10,
+			},
+			expectedID: 1,
 			mock: func(mock *MockusecaseBooksRepository) {
-				mock.EXPECT().Create(createTestCreateBookRequest("Test Title", "Test Description", 10)).Return(nil)
+				mock.EXPECT().ExistsByTitle("Test Title").Return(false, nil)
+				mock.EXPECT().Create(&models.Books{
+					Title:       "Test Title",
+					Description: "Test Description",
+					Qty:         10,
+				}).RunAndReturn(func(book *models.Books) error {
+					book.ID = 1 // hackaround? maybe not the right approach
+					return nil
+				})
 			},
 			wantErr: false,
 		},
+		// RESEARCH: do i need to create individual validation test cases?
 		{
-			name:        "Database error during create",
-			bookRequest: createTestCreateBookRequest("Test Title", "Test Description", 10),
+			name: "Failed create book due to validation errors",
+			bookRequest: &models.CreateBooksRequest{
+				Title:       "in",
+				Description: "in",
+				Qty:         -1,
+			},
 			mock: func(mock *MockusecaseBooksRepository) {
-				mock.EXPECT().Create(createTestCreateBookRequest("Test Title", "Test Description", 10)).Return(nil)
 			},
 			wantErr: true,
-			errType: gorm.ErrInvalidDB,
+			errType: fmt.Errorf("validation error: %w", &models.ValidationError{
+				Message: "invalid input",
+				Errors: map[string]string{
+					"Title":       "Should be at least 3 characters long",
+					"Description": "Should be at least 3 characters long",
+					"Qty":         "Should be greater than 0",
+				},
+			}),
+		},
+		{
+			name: "Failed create book due to title already exists",
+			bookRequest: &models.CreateBooksRequest{
+				Title:       "Test Title",
+				Description: "Test Description",
+				Qty:         10,
+			},
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().ExistsByTitle("Test Title").Return(true, nil)
+			},
+			wantErr: true,
+			errType: models.ErrResourceExistAlready,
+		},
+		{
+			name: "Failed create book due to invalid DB",
+			bookRequest: &models.CreateBooksRequest{
+				Title:       "Test Title",
+				Description: "Test Description",
+				Qty:         10,
+			},
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().ExistsByTitle("Test Title").Return(false, nil)
+				mock.EXPECT().Create(&models.Books{
+					Title:       "Test Title",
+					Description: "Test Description",
+					Qty:         10,
+				}).Return(gorm.ErrInvalidDB)
+			},
+			wantErr: true,
+			errType: fmt.Errorf("repository error: %w", gorm.ErrInvalidDB),
 		},
 	}
 
@@ -63,7 +112,7 @@ func TestCreateBook(t *testing.T) {
 				assert.Equal(t, tt.errType, err)
 			} else {
 				assert.NoError(t, err)
-				assert.Equal(t, tt.expectedID, book.ID) // Assuming the ID is set to 1 in the mock
+				assert.Equal(t, tt.expectedID, book.ID)
 				assert.Equal(t, tt.bookRequest.Title, book.Title)
 				assert.Equal(t, tt.bookRequest.Description, book.Description)
 				assert.Equal(t, tt.bookRequest.Qty, book.Qty)
@@ -72,14 +121,208 @@ func TestCreateBook(t *testing.T) {
 	}
 }
 
-func TestGetBook(t *testing.T) {
+func TestGetBookByID(t *testing.T) {
+	tests := []struct {
+		name         string
+		bookRequest  *models.Books
+		expectedBook *models.BooksSummary
+		id           int
+		mock         func(mock *MockusecaseBooksRepository)
+		wantErr      bool
+		errType      error
+	}{
+		{
+			name:        "Success get book with ID of 1",
+			bookRequest: &models.Books{},
+			expectedBook: &models.BooksSummary{
+				ID:          1,
+				Title:       "Test Title",
+				Description: "Test Description",
+				Qty:         10,
+			},
+			id: 1,
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().GetByID(&models.Books{}, 1).RunAndReturn(func(book *models.Books, id int) error {
+					book.ID = id
+					book.Title = "Test Title"
+					book.Description = "Test Description"
+					book.Qty = 10
+					return nil
+				})
+			},
+			wantErr: false,
+		},
+		{
+			name:        "Failed get book with ID of 99",
+			bookRequest: &models.Books{},
+			expectedBook: &models.BooksSummary{
+				ID:          99,
+				Title:       "Test Title",
+				Description: "Test Description",
+				Qty:         10,
+			},
+			id: 99,
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().GetByID(&models.Books{}, 99).Return(gorm.ErrRecordNotFound)
+			},
+			wantErr: true,
+			errType: fmt.Errorf("repository error: %w", gorm.ErrRecordNotFound),
+		},
+		{
+			name:        "Failed get book with ID due to invalid DB",
+			bookRequest: &models.Books{},
+			expectedBook: &models.BooksSummary{
+				ID:          99,
+				Title:       "Test Title",
+				Description: "Test Description",
+				Qty:         10,
+			},
+			id: 1,
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().GetByID(&models.Books{}, 1).Return(gorm.ErrInvalidDB)
+			},
+			wantErr: true,
+			errType: fmt.Errorf("repository error: %w", gorm.ErrInvalidDB),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mock := NewMockusecaseBooksRepository(t)
+			tt.mock(mock)
+
+			ucValidator := NewCustomValidator(validator.New())
+
+			uc := NewBooksUseCase(mock, ucValidator)
+
+			book, err := uc.GetBookByID(tt.bookRequest, tt.id)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Equal(t, tt.errType, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedBook.ID, book.ID)
+				assert.Equal(t, tt.expectedBook.Title, book.Title)
+				assert.Equal(t, tt.expectedBook.Description, book.Description)
+				assert.Equal(t, tt.expectedBook.Qty, book.Qty)
+			}
+		})
+	}
 }
 
 func TestGetAllBooks(t *testing.T) {
+	tests := []struct {
+		name          string
+		booksRequest  *models.BooksList
+		expectedBooks *models.BooksList
+		available     bool
+		mock          func(mock *MockusecaseBooksRepository)
+		wantErr       bool
+		errType       error
+	}{
+		{
+			name:         "Success get all books",
+			booksRequest: &models.BooksList{},
+			expectedBooks: &models.BooksList{
+				{
+					ID:          1,
+					Title:       "Test Title 1",
+					Description: "Test Description 1",
+					Qty:         10,
+				},
+				{
+					ID:          2,
+					Title:       "Test Title 2",
+					Description: "Test Description 2",
+					Qty:         20,
+				},
+			},
+			available: true,
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().GetAll(&models.BooksList{}).RunAndReturn(func(books *models.BooksList) error {
+					*books = models.BooksList{
+						{
+							ID:          1,
+							Title:       "Test Title 1",
+							Description: "Test Description 1",
+							Qty:         10,
+						},
+						{
+							ID:          2,
+							Title:       "Test Title 2",
+							Description: "Test Description 2",
+							Qty:         20,
+						},
+					}
+					return nil
+				})
+			},
+			wantErr: false,
+		},
+		{
+			name:         "Failed get all books because of invalid parameter",
+			booksRequest: &models.BooksList{},
+			available:    false,
+			mock: func(mock *MockusecaseBooksRepository) {
+			},
+			wantErr: true,
+			errType: fmt.Errorf("invalid parameter"),
+		},
+		{
+			name:         "Failed get all books because no books found in database",
+			booksRequest: &models.BooksList{},
+			available:    true,
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().GetAll(&models.BooksList{}).Return(gorm.ErrRecordNotFound)
+			},
+			wantErr: true,
+			errType: fmt.Errorf("repository error: %w", gorm.ErrRecordNotFound),
+		},
+		{
+			name:         "Failed get all books because of database error",
+			booksRequest: &models.BooksList{},
+			available:    true,
+			mock: func(mock *MockusecaseBooksRepository) {
+				mock.EXPECT().GetAll(&models.BooksList{}).Return(gorm.ErrInvalidDB)
+			},
+			wantErr: true,
+			errType: fmt.Errorf("repository error: %w", gorm.ErrInvalidDB),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mock := NewMockusecaseBooksRepository(t)
+			tt.mock(mock)
+
+			ucValidator := NewCustomValidator(validator.New())
+
+			uc := NewBooksUseCase(mock, ucValidator)
+
+			books, err := uc.GetAllBooks(tt.booksRequest, tt.available)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Equal(t, tt.errType, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, len(*tt.expectedBooks), len(*books))
+				for i, expected := range *tt.expectedBooks {
+					actual := (*books)[i]
+					assert.Equal(t, expected.ID, actual.ID)
+					assert.Equal(t, expected.Title, actual.Title)
+					assert.Equal(t, expected.Description, actual.Description)
+					assert.Equal(t, expected.Qty, actual.Qty)
+				}
+			}
+		})
+	}
 }
 
-func TestUpdateBook(t *testing.T) {
-}
-
-func TestDeleteBook(t *testing.T) {
-}
+//
+// func TestUpdateBook(t *testing.T) {
+// }
+//
+// func TestDeleteBook(t *testing.T) {
+// }
