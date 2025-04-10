e9bb11ab99a112ec2447db5d5245b37b7b1829f4
Administrator <gitlab_admin_c1ee3c@example.com>
2025-04-10 10:26:35 +0700

    added some basic logs to handler for detailed errors

--- internal/inbound/handlers/book_handler.go
+++ internal/inbound/handlers/book_handler.go
@@ -4,6 +4,7 @@ import (
 	"crud-echo/internal/inbound/customvalidator"
 	"crud-echo/internal/models"
 	uc "crud-echo/internal/usecase"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -23,15 +24,18 @@ func (h BooksHandler) CreateBook(c echo.Context) error {
 	var b models.CreateBooksRequest
 
 	if err := c.Bind(&b); err != nil {
+		log.Printf("Error binding request: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.BadRequest)
 	}
 
 	if err := h.cv.Validate(b); err != nil {
+		log.Printf("Error validating request: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.ErrValidationError.Error())
 	}
 
 	_, err := h.buc.CreateBook(&b)
 	if err != nil {
+		log.Printf("Error creating book: %v", err)
 		return echo.NewHTTPError(models.GetErrorHTTPStatusCode(err), models.GetErrorHTTPStatusMessage(err))
 	}
 
@@ -41,11 +45,13 @@ func (h BooksHandler) CreateBook(c echo.Context) error {
 func (h BooksHandler) GetBookByID(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
+		log.Printf("Error converting id to integer: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.BadRequest)
 	}
 
 	resp, err := h.buc.GetBookByID(id)
 	if err != nil {
+		log.Printf("Error retrieving book with ID %d: %v", id, err)
 		return echo.NewHTTPError(models.GetErrorHTTPStatusCode(err), models.GetErrorHTTPStatusMessage(err))
 	}
 
@@ -56,11 +62,13 @@ func (h BooksHandler) GetBookByID(c echo.Context) error {
 func (h BooksHandler) GetAllBooks(c echo.Context) error {
 	available, err := strconv.ParseBool(c.QueryParam("available"))
 	if err != nil {
+		log.Printf("Error parsing available param: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.BadRequest)
 	}
 
 	resp, err := h.buc.GetAllBooks(available)
 	if err != nil {
+		log.Printf("Error retrieving books: %v", err)
 		return echo.NewHTTPError(models.GetErrorHTTPStatusCode(err), models.GetErrorHTTPStatusMessage(err))
 	}
 
@@ -72,14 +80,17 @@ func (h BooksHandler) UpdateBook(c echo.Context) error {
 	var b models.UpdateBooksRequest
 
 	if err := c.Bind(&b); err != nil {
+		log.Printf("Error binding request: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.BadRequest)
 	}
 
 	if err := h.cv.Validate(b); err != nil {
+		log.Printf("Error validating request: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.ErrValidationError.Error())
 	}
 
 	if err := h.buc.UpdateBook(&b); err != nil {
+		log.Printf("Error updating book: %v", err)
 		return echo.NewHTTPError(models.GetErrorHTTPStatusCode(err), models.GetErrorHTTPStatusMessage(err))
 	}
 
@@ -90,14 +101,17 @@ func (h BooksHandler) DeleteBook(c echo.Context) error {
 	var b models.DeleteBooksRequest
 
 	if err := c.Bind(&b); err != nil {
+		log.Printf("Error binding request: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.BadRequest)
 	}
 
 	if err := h.cv.Validate(b); err != nil {
+		log.Printf("Error validating request: %v", err)
 		return echo.NewHTTPError(http.StatusBadRequest, models.ErrValidationError.Error())
 	}
 
 	if err := h.buc.DeleteBook(&b); err != nil {
+		log.Printf("Error deleting book: %v", err)
 		return echo.NewHTTPError(models.GetErrorHTTPStatusCode(err), models.GetErrorHTTPStatusMessage(err))
 	}
 
